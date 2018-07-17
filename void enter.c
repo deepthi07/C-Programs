42a9ce2276d974bb36dad9c23e19c4d75521e965
deepthi07 <deepthi.thani@gmail.com>
2018-07-17 19:07:30 +0200

    Add code for pyramid, euler problems

---
+++ void enter.c
@@ -0,0 +1,14 @@
+void enter()
+{
+int i;
+for(i=0; i<20; i++) {
+cout << “Last name: “;
+cin >> name[i];
+cout << “Phone number: “;
+cin >> phone[i];
+cout << “Hours worked: “;
+cin >> hours[i];
+cout << “Wage: “;
+cin >> wage[i];
+}
+}
